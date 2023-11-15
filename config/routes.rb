f1995074c16bfbb7b9530187b67a5fa3caed1bd6
john steisel <john.steisel@gmail.com>
2023-11-15 15:37:33 +0100

    Challenge 2 completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  delete 'restaurants/:id', to: "restaurants#destroy", as: :delete_from_index
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+restaurant = Restaurant.create(name: "Piola", address: "Ixelles", phone_number: "040404040404", category:"italian")
+puts "created #{restaurant.name}"
+restaurant = Restaurant.create(name: "Thai café", address: "Bxl", phone_number: "040404040404", category:"chinese")
+puts "created #{restaurant.name}"
+restaurant = Restaurant.create(name: "SushiShop", address: "Woluwe", phone_number: "040404040404", category:"japanese")
+puts "created #{restaurant.name}"
